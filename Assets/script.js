d312a3ef4cd55e674363da462cbc6275d4125122
Joselyn Zuniga <joselynzuniga@joselyns-mbp.attlocal.net>
2023-02-21 23:39:33 -0600

    updated html, css, and js

---
+++ Assets/script.js
@@ -0,0 +1,15 @@
+let timeLeft = document.querySelector(".time-left");
+let questionContainer = document.getElementsByClassName("question-container");
+let nxtButton = document.getElementById("next");
+let numberofQuestions = document.querySelector(".number-of-questions");
+let Container = document.getElementsByClassName("container");
+let scoreKeeper = document.getElementById("score-keeper");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startBtn = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
+
+ 
